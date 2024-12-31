50f6830974e02e1a9130df38cbf0f5dd0f26989b
Neon Abyss <neonabyss00@gmail.com>
2024-12-31 05:32:17 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: Venocyber_Tech,    useMultiFileAuthState,
+    default: N3onAbyss,    useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
     Browsers
@@ -19,7 +19,7 @@ function removeFile(FilePath){
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function VENOCYBER_MD_PAIR_CODE() {
+        async function NEON_MD_PAIR_CODE() {
         const {
             state,
             saveCreds
@@ -34,16 +34,16 @@ router.get('/', async (req, res) => {
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: ["Chrome (Linux)", "", ""]
              });
-             if(!Pair_Code_By_Venocyber_Tech.authState.creds.registered) {
+             if(!Created_by_Neon_Abyss.authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_Venocyber_Tech.requestPairingCode(num)
+                            const code = await Pair_Code_By_Neon_Abyss.requestPairingCode(num)
                  if(!res.headersSent){
                  await res.send({code});
                      }
                  }
-            Pair_Code_By_Venocyber_Tech.ev.on('creds.update', saveCreds)
-            Pair_Code_By_Venocyber_Tech.ev.on("connection.update", async (s) => {
+            Pair_Code_By_Neon_Abyss.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Neon_Abyss.ev.on("connection.update", async (s) => {
                 const {
                     connection,
                     lastDisconnect
@@ -53,37 +53,37 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Venocyber_Tech.sendMessage(Pair_Code_By_Venocyber_Tech.user.id, { text: '' + b64data });
+               let session = await Pair_Code_By_Neon_Abyss.sendMessage(Pair_Code_By_Neon_Abyss.user.id, { text: '' + b64data });
 
-               let VENOCYBER_MD_TEXT = `
-*_Pair Code Connected by Venocyber Tech_*
+               let NEON_MD_TEXT = `
+*_Pair Code Connected by Neon Abyss_*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU CHOOSEN VENOCYBER-MD 』*
+║ *『 WOW YOU CHOOSEN NEON-MD 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚══════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@JASTINMTEWA-vn9pl_
-║❒ *Owner:* _https://wa.me/message/A4QG2JZKBXFTN1_
-║❒ *Repo:* _https://github.com/Kingjux/venocyber-md_
-║❒ *WaGroup:* _https://chat.whatsapp.com/HSln3blDuuuKvC8njxyCCN_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaYauR9ISTkHTj4xvi1l_
-║❒ *Plugins:* _https://github.com/Kingjux/venocyber-md-plugins_
+║❒ *Ytube:* _CominSoon_
+║❒ *Owner:* _https://wa.me/263713431767_
+║❒ *Repo:* _https://github.com/N3onAbyss/NEON-MD_
+║❒ *WaGroup:* _https://whatsapp.com/channel/0029Vb0mA2i3QxS2C1kvFp3p_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029Vb0mA2i3QxS2C1kvFp3p_
+║❒ *Plugins:* _https://github.com/N3onAbyss/neon-md-plugins_
 ╚══════════════════════╝ 
 _____________________________________
 
 _Don't Forget To Give Star To My Repo_`
- await Pair_Code_By_Venocyber_Tech.sendMessage(Pair_Code_By_Venocyber_Tech.user.id,{text:VENOCYBER_MD_TEXT},{quoted:session})
+ await Pair_Code_By_Neon_Abyss.sendMessage(Pair_Code_By_Neon_Abyss.user.id,{text:NEON_MD_TEXT},{quoted:session})
  
 
         await delay(100);
-        await Pair_Code_By_Venocyber_Tech.ws.close();
+        await Pair_Code_By_Neon_Abyss.ws.close();
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    VENOCYBER_MD_PAIR_CODE();
+                    NEON_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
@@ -94,6 +94,6 @@ _Don't Forget To Give Star To My Repo_`
          }
         }
     }
-    return await VENOCYBER_MD_PAIR_CODE()
+    return await NEON_MD_PAIR_CODE()
 });
 module.exports = router
